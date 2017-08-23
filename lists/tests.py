c289bf7c8f0c980bbe1b6cb904ea721a4d44e1c3
Jefferson Ridgeway <jeffersonridgeway@Jeffersons-MacBook-Pro.local>
2017-08-23 17:04:22 -0400

    made changes to functional_tests.py, home.html, and tests.py and views.py

--- lists/tests.py
+++ lists/tests.py
@@ -17,3 +17,18 @@ class HomePageTest(TestCase):
         response = home_page(request)
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        #setup test
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        #exercise/execute test
+        response = home_page(request)
+        #assert test
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'}
+        )
+        self.assertEqual(response.content.decode(), expected_html)
